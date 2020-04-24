144747075325310cf34ebbd2a5a4261a9696c336
Zinkevich <kevichivan@gmail.com>
2020-04-24 19:54:23 +0300

    add data types

---
+++ dataTypes.js
@@ -0,0 +1,49 @@
+/**  Data Types:
+undefined - hasn't been defined
+null - nothing
+boolean
+string
+symbol - uniq value
+number
+object
+*/
+
+// var - through out all program
+var myName = "Ivan"
+
+myName = 8;
+
+// let available only in scope
+let ourName = "freeCodeCamp"
+
+// const - should never change
+const pi = 3.14
+
+// declaring variable
+var a;
+
+// assignment variable
+var b = 2;
+
+a = 7;
+b = a;
+console.log(b);
+
+var sum = 10 + 10;
+
+// Support for (In/De)crementing
+sum++;
+sum--;
+
+sum += 2.4
+console.log(sum);
+
+// DAFAC?!!??!?!?!?
+var floatMultiply = 2.0 * 2.5;
+console.log(floatMultiply);
+
+var myStr = "laksd \"alsm\t\" \b\ndla"
+console.log(myStr)
+
+// find length of string 
+console.log(myStr.length)
